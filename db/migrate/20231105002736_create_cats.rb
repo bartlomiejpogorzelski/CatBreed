b943ba0915dd424b03b17746af3d8bfd34d47b1a
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2023-11-05 01:48:43 +0100

    Create model and migration Cat

---
+++ db/migrate/20231105002736_create_cats.rb
@@ -0,0 +1,25 @@
+class CreateCats < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.string :breed
+      t.string :color
+      t.date :date_of_birth
+      t.string :gender
+      t.text :description
+      t.string :images
+      t.string :pedigree_information
+      t.decimal :price
+      t.string :status
+      t.string :vaccination_information
+      t.string :health_status
+      t.string :owner_information
+      t.string :location
+      t.boolean :neutered
+      t.text :notes
+      t.string :videos
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_05_002736) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.string "color"
+    t.date "date_of_birth"
+    t.string "gender"
+    t.text "description"
+    t.string "images"
+    t.string "pedigree_information"
+    t.decimal "price"
+    t.string "status"
+    t.string "vaccination_information"
+    t.string "health_status"
+    t.string "owner_information"
+    t.string "location"
+    t.boolean "neutered"
+    t.text "notes"
+    t.string "videos"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
