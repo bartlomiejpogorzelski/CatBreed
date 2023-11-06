2528ce70f254fbfc2a34c452faf2f315cd2a9e37
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2023-11-07 00:52:14 +0100

    Change home to viewComponent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+
+  append_view_path Rails.root.join("app", "components")
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
+  
   def index
-   
+    render HomeComponent.new
   end
 end
