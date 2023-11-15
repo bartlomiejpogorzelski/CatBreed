3dcb69e996a4c1990704b29f64030b63f8b0de05
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2023-11-15 20:12:35 +0100

    Feature/mailer -> master (2) (#3)
    * Configuration mailer
    
    * Change config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,14 +71,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mailtrap.io',
-    port: 2525,
-    user_name: 'mailtrap_username',
-    password: 'ymailtrap_password',
-    authentication: :cram_md5
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.mailtrap.io',
+  #   port: 2525,
+  #   user_name: 'mailtrap_username',
+  #   password: 'ymailtrap_password',
+  #   authentication: :cram_md5
+  # }
 
 
 end
