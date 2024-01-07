4fed2bdac15640e13cc74f00bb28eb44a1a91e8e
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2024-01-07 21:08:34 +0100

    Add option to change language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
   append_view_path Rails.root.join("app", "components")
+
+  private 
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
