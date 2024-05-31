ea8749fc8fde5125ae8b7f44b3db017a6377a4a7
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2024-05-31 20:33:05 +0200

    Add optional in model. Change validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
   include ImageUploader::Attachment(:image)
-  belongs_to :cat
-  belongs_to :post
+  belongs_to :cat, optional: true
+  belongs_to :post, optional: true
 end
