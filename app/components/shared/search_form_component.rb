9466e3abf72375bb965439243ef97b18e396c8e5
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2024-07-08 18:52:03 +0200

    Create default ransack fitler

--- app/components/shared/search_form_component.rb
+++ app/components/shared/search_form_component.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class Shared::SearchFormComponent < ViewComponent::Base
-
   attr_reader :f
 
   def initialize(f:)
     @f = f
   end
 
+  private
+
+  def neutered_checked
+    params[:q].present? && ActiveRecord::Type::Boolean.new.cast(params[:q][:neutered_eq])
+  end
 end
