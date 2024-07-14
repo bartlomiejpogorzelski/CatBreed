803907ec4139a87d8859abf37c34aab4476cf534
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2024-07-14 23:17:47 +0200

    Add option to cancel reservation (#29)

--- spec/controllers/cats/reservations_controller_spec.rb
+++ spec/controllers/cats/reservations_controller_spec.rb
@@ -6,48 +6,67 @@ RSpec.describe Cats::ReservationsController, type: :controller do
   let(:invalid_attributes) { attributes_for(:reservation, cat_id: cat.id, start_date: nil) }
   let(:reservation) { create(:reservation, cat_id: cat.id, start_date: "2023-11-23T12:12")  }
 
-  describe "with valid params" do
+  describe "POST #create" do
+    context "with valid params" do
+      it 'creates a new reservation' do
+        expect do
+          post :create, params: { reservation: valid_attributes }
+        end.to change(Reservation, :count).by(1)
+      end
 
-    it 'creates a new reservation' do
-      expect do
+      it 'updates cat status to reservation_reported' do
         post :create, params: { reservation: valid_attributes }
-      end.to change(Reservation, :count).by(1)
+        cat.reload
+        expect(cat.status).to eq('reservation_reported')
+      end
     end
 
-    it 'updates cat status to reservation_reported' do
-      post :create, params: { reservation: valid_attributes }
-      cat.reload
-      expect(cat.status).to eq('reservation_reported')
-    end
-
-  end
-
-  describe "with not valid params" do
-
-    it 'creates a new reservation' do
-      expect do
-        post :create, params: { reservation: invalid_attributes }
-      end.to change(Reservation, :count).by(0)
+    context "with invalid params" do
+      it 'does not create a new reservation' do
+        expect do
+          post :create, params: { reservation: invalid_attributes }
+        end.to change(Reservation, :count).by(0)
+      end
     end
   end
 
-
-  describe 'PATCH #update' do
-
-    context 'with valid params' do
-      it 'updates the reservation' do
+  describe "PATCH #update" do
+    context "with valid params to accept reservation" do
+      it 'updates the reservation to deposit_paid: true' do
         patch :update, params: { id: reservation.id, deposit_paid: true }, format: :turbo_stream
         reservation.reload
         expect(reservation.deposit_paid).to eq(true)
       end
-  
-      it 'updates the cat status' do
-        expect do
-          patch :update, params: { id: reservation.id, deposit_paid: true }, format: :turbo_stream
-          reservation.cat.reload
-        end.to change(reservation.cat, :status).to('reserved')
+
+      it 'updates the cat status to reserved' do
+        patch :update, params: { id: reservation.id, deposit_paid: true }, format: :turbo_stream
+        reservation.cat.reload
+        expect(reservation.cat.status).to eq('reserved')
+      end
+
+      it 'renders the turbo_stream template' do
+        patch :update, params: { id: reservation.id, deposit_paid: true }, format: :turbo_stream
+        expect(response.media_type).to eq Mime[:turbo_stream].to_s
+      end
+    end
+
+    context "with valid params to cancel reservation" do
+      it 'updates the reservation to deposit_paid: false' do
+        patch :update, params: { id: reservation.id, deposit_paid: false }, format: :turbo_stream
+        reservation.reload
+        expect(reservation.deposit_paid).to eq(false)
+      end
+
+      it 'updates the cat status to available' do
+        patch :update, params: { id: reservation.id, deposit_paid: false }, format: :turbo_stream
+        reservation.cat.reload
+        expect(reservation.cat.status).to eq('available')
+      end
+
+      it 'renders the turbo_stream template' do
+        patch :update, params: { id: reservation.id, deposit_paid: false }, format: :turbo_stream
+        expect(response.media_type).to eq Mime[:turbo_stream].to_s
       end
     end
   end
 end
-
