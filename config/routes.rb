2f75e8dae42e06996e88c3a5bdab0785a0e05bbc
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-07-13 15:28:59 +0200

    Add funcionality for cart shopping

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,15 @@ Rails.application.routes.draw do
   resources :quizzes, only: [:index] do
     post :submit_answer, on: :member
   end
+
+  namespace :ecommerce do
+    resource :cart, only: [:show], controller: 'cart' do
+      post 'add/:product_id', to: 'cart#add', as: 'add'
+      delete 'remove/:product_id', to: 'cart#remove', as: 'remove'
+      delete 'clear', to: 'cart#clear', as: 'clear'
+    end
+  end
+
   get "/pages/:page" => "pages#show",  as: 'pages'
   get '/unauthorized', to: 'unauthorized#show', as: 'unauthorized'
 end
