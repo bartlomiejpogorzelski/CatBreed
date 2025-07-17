ea95c2fe2f018142822f3a471fa8f55089ee5183
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2025-07-17 19:09:50 +0200

    Fixes in CartController

--- app/controllers/ecommerce/cart_controller.rb
+++ app/controllers/ecommerce/cart_controller.rb
@@ -1,10 +1,7 @@
 class Ecommerce::CartController < ApplicationController
-  before_action :set_cart
+  before_action :set_cart, only: %i[show add remove clear]
 
   def show
-    @items = @cart.items
-    @total_price = @cart.total_price
-
     render Ecommerce::CartComponent.new(
       items: @cart.items,
       total_price: @cart.total_price
@@ -12,18 +9,19 @@ class Ecommerce::CartController < ApplicationController
   end
 
   def add
-    @cart.add_product(params[:product_id], params[:quantity].to_i > 0 ? params[:quantity].to_i : 1)
-    redirect_to ecommerce_cart_path, notice: "Produkt dodany do koszyka."
+    quantity = [params[:quantity].to_i, 1].max
+    @cart.add_product(params[:product_id], quantity)
+    redirect_to ecommerce_cart_path #, notice: "Produkt dodany do koszyka."
   end
 
   def remove
     @cart.remove_product(params[:product_id])
-    redirect_to ecommerce_cart_path, notice: "Produkt usunięty z koszyka."
+    redirect_to ecommerce_cart_path #, notice: "Produkt usunięty z koszyka."
   end
 
   def clear
     @cart.clear
-    redirect_to ecommerce_cart_path, notice: "Koszyk został wyczyszczony."
+    redirect_to ecommerce_cart_path #, notice: "Koszyk został wyczyszczony."
   end
 
   private
